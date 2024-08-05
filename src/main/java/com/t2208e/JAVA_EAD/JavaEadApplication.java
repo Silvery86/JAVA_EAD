d371f66b8629b17d38a31deb70d32355227ab69f
giang8692@gmail.com <giang8692@gmail.com>
2024-08-05 18:47:16 +0700

    upload project

---
+++ src/main/java/com/t2208e/JAVA_EAD/JavaEadApplication.java
@@ -0,0 +1,13 @@
+package com.t2208e.JAVA_EAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t2208e/JAVA_EAD/JavaEadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t2208e.JAVA_EAD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
